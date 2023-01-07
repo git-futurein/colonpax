3e5b307b5eb67fe96ef723b41edd1274fd7f7a79
Zarifprogrammer <zarifprogrammer@gmail.com>
2023-01-07 11:57:04 +0600

    Lead Generation done

--- public/api/leads.php
+++ public/api/leads.php
@@ -5,14 +5,14 @@ header('Access-Control-Allow-Origin: *');
 include('./partial/_config.php');
 
 $data = $_GET;
-$url = 'https://api.konnektive.com/order/import/?loginId=' . $loginId . '&password=' . $loginKey;
+$url = 'https://api.konnektive.com/leads/import/?loginId=' . $loginId . '&password=' . $loginKey;
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
     CURLOPT_URL => $url,
-    CURLOPT_POSTFIELDS => $data,
     CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => $data,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_TIMEOUT => 0,
